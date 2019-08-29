cecd69ad6119592c447b9c1c5b841c10b7b27e88
Jose Salgado <Josalgado03@gmail.com>
2019-08-29 02:01:11 -0700

    Added  basic code to take information from the Add form and input it to the database. Database is successfully storing the new firearm.

--- TeamSourceControl/frmAddFirearm.cs
+++ TeamSourceControl/frmAddFirearm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,29 @@ namespace TeamSourceControl
         public frmAddFirearm()
         {
             InitializeComponent();
+        }
+
+        private void btnAddFirearm_Click(object sender, EventArgs e)
+        {
+            Firearms f = new Firearms()
+            {
+                SerialNumber = txtSerialNumber.Text,
+                Manufacturer = txtManufacturer.Text,
+                ModelName = txtModel.Text,
+                Caliber = txtCaliper.Text,
+                Price = Convert.ToDecimal(txtPrice.Text)
+            };
+
+            try
+            {
+                FirearmDB.AddFirearm(f);
+                MessageBox.Show("Firearm was added!");
+                Close();
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Database Error", "There was an issue when trying to add to the DB");
+            }
         }
     }
 }
