29f04045ced9ba17700bc53b81cfdead18d43c7c
CalvinSithideth <calvin.sithideth@gmail.com>
2019-08-29 22:02:09 -0700

    Updated the update-form with labels and an extra txtBox for the primary key

--- TeamSourceControl/formUpdateFirearm.Designer.cs
+++ TeamSourceControl/formUpdateFirearm.Designer.cs
@@ -34,6 +34,13 @@
             this.txtUpdateModel = new System.Windows.Forms.TextBox();
             this.txtUpdateCaliber = new System.Windows.Forms.TextBox();
             this.txtUpdatePrice = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPrimaryKey = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnUpdateFirearm
@@ -81,11 +88,80 @@
             this.txtUpdatePrice.Size = new System.Drawing.Size(178, 20);
             this.txtUpdatePrice.TabIndex = 4;
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(76, 199);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Price:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(68, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Caliber:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Model Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Manufacturer:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Serial Number:";
+            // 
+            // txtPrimaryKey
+            // 
+            this.txtPrimaryKey.Enabled = false;
+            this.txtPrimaryKey.Location = new System.Drawing.Point(116, 65);
+            this.txtPrimaryKey.Name = "txtPrimaryKey";
+            this.txtPrimaryKey.Size = new System.Drawing.Size(178, 20);
+            this.txtPrimaryKey.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(45, 68);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Primary Key:";
+            // 
             // FormUpdateFirearm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtPrimaryKey);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.txtUpdatePrice);
             this.Controls.Add(this.txtUpdateCaliber);
             this.Controls.Add(this.txtUpdateModel);
@@ -107,5 +183,12 @@
         private System.Windows.Forms.TextBox txtUpdateModel;
         private System.Windows.Forms.TextBox txtUpdateCaliber;
         private System.Windows.Forms.TextBox txtUpdatePrice;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPrimaryKey;
+        private System.Windows.Forms.Label label6;
     }
 }
