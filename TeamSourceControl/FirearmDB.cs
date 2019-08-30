d9ed1e1e8b06a534ff915ea746ac6247e850fcea
Jose Salgado <Josalgado03@gmail.com>
2019-08-29 18:51:57 -0700

    Partially through the update sequence. Encountered an error with EntityValidationErrors. Have done research and realized I am missing fields. Added a try catch with EntityValidation error printouts to find out whats wrong (line 64 - 83)

--- TeamSourceControl/FirearmDB.cs
+++ TeamSourceControl/FirearmDB.cs
@@ -61,10 +61,30 @@ namespace TeamSourceControl
         {
             using (var context = new SimpleGunShopDatabaseEntities())
             {
-                context.Firearms.Add(firearm);
-                context.Entry(firearm).State = EntityState.Modified; // Tell EF it has been modified
-                context.SaveChanges();
-                return firearm;
+                try
+                {
+                    context.Firearms.Add(firearm);
+                    context.Entry(firearm).State = EntityState.Modified; // Tell EF it has been modified
+                    context.SaveChanges();
+                    return firearm;
+                }
+                catch (System.Data.Entity.Validation.DbEntityValidationException dbEx)
+                {
+                    Exception raise = dbEx;
+                    foreach (var validationErrors in dbEx.EntityValidationErrors)
+                    {
+                        foreach(var validationError in validationErrors.ValidationErrors)
+                            {
+                            string message = string.Format("{0}:{1}",
+                                validationErrors.Entry.Entity.ToString(),
+                                validationError.ErrorMessage);
+
+                            raise = new InvalidOperationException(message, raise);
+                        }
+                    }
+                    throw raise;
+                }
+                
             }
         }
 
