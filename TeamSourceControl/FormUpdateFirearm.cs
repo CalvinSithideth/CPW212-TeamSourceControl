e4a0f71e59edc84cde64107ad7da73067078913a
Jose Salgado <Josalgado03@gmail.com>
2019-08-29 18:09:20 -0700

    Selected firearm information is appearing on the update form when gun is selected and update is clicked.

--- TeamSourceControl/FormUpdateFirearm.cs
+++ TeamSourceControl/FormUpdateFirearm.cs
@@ -12,14 +12,29 @@ namespace TeamSourceControl
 {
     public partial class FormUpdateFirearm : Form
     {
+        //Set a private field
+        private Firearms existingFirearm;
+
         public FormUpdateFirearm(Firearms f = null)
         {
             InitializeComponent();
+            existingFirearm = f;
+            if (f != null)
+            {
+                Text = "Updating firearm : " + f.SerialNumber;
+                //Populate existing firearm on the update form with main form
+                txtUpdateSerialNum.Text = existingFirearm.SerialNumber;
+                txtUpdateManufacturer.Text = existingFirearm.Manufacturer;
+                txtUpdateModel.Text = existingFirearm.ModelName;
+                txtUpdateCaliber.Text = existingFirearm.Caliber;
+                txtUpdatePrice.Text = Convert.ToString(existingFirearm.Price);
+            }
+
         }
 
         private void btnUpdateFirearm_Click(object sender, EventArgs e)
         {
-         
+
         }
     }
 }
